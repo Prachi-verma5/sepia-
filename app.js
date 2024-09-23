2481fbeb200c8408dc7ad6f9404d6c762bf2ae08
Prachi-verma5 <vermaprachi0k30@gmail.com>
2024-09-23 20:12:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// let next = document.querySelector('.next')
+// let previous = document.querySelector('.previous')
+
+// next.addEventListener('click', function() {
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slider').appendChild(items[0])  
+// })
+
+// previous.addEventListener('click', function() {
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slider').prepend(items[items.length - 1])  
+// })
